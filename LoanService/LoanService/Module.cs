8c5fa6a82e98f85959ffd4c481e5e003380b4353
ragsrams7 <raghuram.nitw@gmail.com>
2016-05-23 23:59:42 -0400

    Add files via upload

---
+++ LoanService/LoanService/Module.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace LoanService
+{
+    public class Module : IHttpModule
+    {
+        public void Init(HttpApplication applicationcontext)
+        {
+            applicationcontext.PreSendRequestContent += new EventHandler(Applicationcontext_PreSendRequestContent);
+            applicationcontext.PreRequestHandlerExecute += new EventHandler(Applicationcontext_PreRequestHandlerExecute);
+            applicationcontext.BeginRequest += new EventHandler(Applicationcontext_BeginRequest);
+            applicationcontext.EndRequest += new EventHandler(Applicationcontext_EndRequest);
+
+        }
+
+        private void Applicationcontext_EndRequest(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Applicationcontext_BeginRequest(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Applicationcontext_PreSendRequestContent(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void Applicationcontext_PreRequestHandlerExecute(object sender, EventArgs e)
+        {
+            HttpRequest request = ((HttpApplication)sender).Request;
+            HttpContext context = ((HttpApplication)sender).Context;
+            Stream HttpStream = context.Request.InputStream;
+            HttpStream.Position = 0;
+            string filePath = context.Request.FilePath;
+            string fileExtension =
+                VirtualPathUtility.GetExtension(filePath);
+            if (fileExtension.Equals(".svc"))
+            {
+
+            }
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
