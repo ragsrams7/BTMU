b07485de4ae28d5d72c5f740620799cecc27afcd
ragsrams7 <raghuram.nitw@gmail.com>
2019-02-21 22:04:56 -0500

    Add files via upload
     services.AddDbContext<LivePointsDBContext>(options =>
                          options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
                services.AddDbContext<LivePointsDBContext>();

---
+++ LivePointsDBContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DALCore;
+using DALCore.Models;
+
+namespace LivePoints.Repository
+{
+    public class LivePointsDBContext : DbContext
+    {
+        public LivePointsDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Driver> Drivers { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<ShopUserMapping> ShopUserMappings { get; set; }
+        public DbSet<Transactions> Transactionss { get; set; }
+        public DbSet<ClaimHistory> ClaimHistorys { get; set; }
+        public DbSet<Notification> Notifications { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+        public DbSet<ShopAd> ShopAds { get; set; }
+        public object SchemaName { get; private set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasDefaultSchema(schema: DBGlobals.SchemaName);
+            modelBuilder.Entity<User>().ToTable("User");
+            modelBuilder.Entity<Driver>().ToTable("Driver");
+            modelBuilder.Entity<Shop>().ToTable("Shop");
+            modelBuilder.Entity<ShopUserMapping>().ToTable("ShopUserMapping");
+            modelBuilder.Entity<Transactions>().ToTable("Transactions");
+            modelBuilder.Entity<ClaimHistory>().ToTable("ClaimHistory");
+            modelBuilder.Entity<Notification>().ToTable("Notification");
+            modelBuilder.Entity<Review>().ToTable("Review");
+            modelBuilder.Entity<ShopAd>().ToTable("ShopAd");
+
+            base.OnModelCreating(modelBuilder);
+            //builder.Entity<User>()
+            //    .HasOne(b => b.User)
+            //    .WithMany(p => p.Posts)
+            //    .IsRequired();
+            //base.OnModelCreating(builder);
+        }
+
+    }
+}
---
+++ LivePointsDBContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using LivePoints.Repository;
+
+namespace LivePoints.DBRepository
+{
+    public class LivePointsDBContextFactory : IDesignTimeDbContextFactory<LivePointsDBContext>
+    {
+        public LivePointsDBContextFactory()
+        {
+
+        }
+        public LivePointsDBContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<LivePointsDBContext>();
+            //builder.UseSqlServer(
+            //    @"Server=(localdb)\mssqllocaldb;Database=LivePoints;Trusted_Connection=True;MultipleActiveResultSets=true;");
+            builder.UseSqlServer(
+                @"Server=sql5021.site4now.net;Database=DB_9B4C66_LivePoints;Trusted_Connection=False;MultipleActiveResultSets=true; uid=DB_9B4C66_LivePoints_admin; pwd=Origin#130;");
+           
+            return new LivePointsDBContext(builder.Options);
+        }
+    }
+}
